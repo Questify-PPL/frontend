ebfbc5f2df4e1cd640be6c23457ab12397a4b3b5
eddoog <alfredo.austin@ui.ac.id>
2024-02-19 01:41:48 +0700

    test: adjust and add missing test

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -3,10 +3,23 @@ import { render, screen } from "@testing-library/react";
 import Page from "@/app/page";
 
 describe("Page", () => {
-  it("renders without crashing", () => {
+  it("renders the hero section", () => {
     render(<Page />);
-    const text = screen.getByText("This is Questify Staging");
+    const text = screen.getByText(/Empower academic communities engagement/i);
+    const button = screen.getByText(/Get Started/i);
 
     expect(text).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders the end section", () => {
+    render(<Page />);
+    const text = screen.getByText(
+      /Unlock academic success with our streamlined form builder and distributor/i
+    );
+    const button = screen.getByText(/Sign Up/i);
+
+    expect(text).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 });
