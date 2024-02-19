1237f4ef20bd521979912b6f0b25e4f9b3550ab4
eddoog <alfredo.austin@ui.ac.id>
2024-02-19 13:37:00 +0700

    refactor: refactor benefit card and format code

--- components/landing-page/Benefit.tsx
+++ components/landing-page/Benefit.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Head } from "./Head";
 import Image from "next/image";
 import { BENEFIT } from "@/lib/constant";
+import { BenefitCard } from "./BenefitCard";
 
 export function Benefit() {
   return (
@@ -12,35 +13,11 @@ export function Benefit() {
       <Head content="Create an educationally optimal framework for diverse professional questionnaires" />
       {BENEFIT.map((benefit, index) => {
         return (
-          <div
-            className={`flex sm:gap-8 gap-2 w-full items-center overflow-hidden pb-4 pt-4 lg:pb-8 lg:pt-16 ${index % 2 == 0 ? "flex-row" : "flex-row-reverse"}`}
+          <BenefitCard
             key={`benefit-${index + 1}`}
-          >
-            <Image
-              src={benefit.src}
-              alt={`Benefit ${index + 1}`}
-              width={239}
-              height={180}
-              className="hidden sm:block"
-            />
-            <Image
-              src={benefit.src}
-              alt={`Benefit ${index + 1}`}
-              width={119}
-              height={90}
-              className="block sm:hidden"
-            />
-            <div
-              className={`flex flex-col space-y-2 text-primary sm:w-fit w-3/4 ${index % 2 == 0 ? "" : "items-end"}`}
-            >
-              <h2 className="font-bold lg:text-3xl md:text-2xl text-xl sm:block hidden">
-                {benefit.title}
-              </h2>
-              <p className="font-medium lg:text-2xl md:text-xl sm:text-lg text-xs">
-                {benefit.description}
-              </p>
-            </div>
-          </div>
+            benefit={benefit}
+            index={index}
+          />
         );
       })}
     </section>
--- components/landing-page/BenefitCard.tsx
+++ components/landing-page/BenefitCard.tsx
@@ -1,3 +1,40 @@
-export function BenefitCard() {
-  return;
+import Image from "next/image";
+
+export function BenefitCard({
+  benefit,
+  index,
+}: {
+  benefit: { src: string; title: string; description: string };
+  index: number;
+}) {
+  return (
+    <div
+      className={`flex sm:gap-8 gap-2 w-full items-center overflow-hidden pb-4 pt-4 lg:pb-8 lg:pt-16 ${index % 2 == 0 ? "flex-row" : "flex-row-reverse"}`}
+    >
+      <Image
+        src={benefit.src}
+        alt={`Benefit ${index + 1}`}
+        width={239}
+        height={180}
+        className="hidden sm:block"
+      />
+      <Image
+        src={benefit.src}
+        alt={`Benefit ${index + 1}`}
+        width={119}
+        height={90}
+        className="block sm:hidden"
+      />
+      <div
+        className={`flex flex-col space-y-2 text-primary sm:w-fit w-3/4 ${index % 2 == 0 ? "" : "items-end"}`}
+      >
+        <h2 className="font-bold lg:text-3xl md:text-2xl text-xl sm:block hidden">
+          {benefit.title}
+        </h2>
+        <p className="font-medium lg:text-2xl md:text-xl sm:text-lg text-xs">
+          {benefit.description}
+        </p>
+      </div>
+    </div>
+  );
 }
