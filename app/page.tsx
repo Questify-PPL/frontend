da9be07fba21bdd830188d42826585d861655944
Alfredo Austin <alfredo.austin@ui.ac.id>
2024-02-26 20:12:36 +0700

    Revert "Merge branch 'PBI-2-Register' into 'main'"
    This reverts merge request !1

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import { Benefit, End, Hero } from "@/components/landing-page";
-import { Footer, Navbar } from "@/components/header";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main className="flex min-h-screen h-fit flex-col items-center px-2">
-        <Hero />
-        <Benefit />
-        <End />
-      </main>
-      <Footer />
-    </>
+    <main className="flex min-h-screen h-fit flex-col items-center px-2">
+      <Hero />
+      <Benefit />
+      <End />
+    </main>
   );
 }
