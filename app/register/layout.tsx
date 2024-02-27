1ec3db429ee7564e5fa6bcceaa406d6b9777b15d
eddoog <alfredo.austin@ui.ac.id>
2024-02-27 15:56:25 +0700

    chores: fix ui layout

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -7,7 +7,7 @@ export default function RegisterLayout({
 }) {
   return (
     <section className="w-full flex flex-col md:flex-row items-center justify-center h-full min-h-fit">
-      <div className="flex flex-col px-[20px] md:w-fit w-full py-[30px] md:py-[190px] md:min-h-screen self-stretch items-center justify-center md:px-[48px] bg-register-pattern bg-cover bg-no-repeat">
+      <div className="flex flex-col px-[20px] md:w-fit w-full py-[30px] md:py-[0px] md:min-h-screen self-stretch items-center justify-center md:px-[48px] bg-register-pattern bg-cover bg-no-repeat">
         <TriviaForm />
       </div>
       {children}
