cd8f6f53d8adae97723058d3d9f4d2452a04d768
Kenneth Ferdinand <kenneth.ferdinand@ui.ac.id>
2024-02-27 20:28:22 +0700

    Fixing tests

--- __tests__/register/verification/verify.test.tsx
+++ __tests__/register/verification/verify.test.tsx
@@ -1,7 +1,6 @@
 import Verify from "@/app/register/verification/page";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom"
-import React from "react";
+import "@testing-library/jest-dom";
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -9,53 +8,69 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
   disconnect: jest.fn(),
 }));
 
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      status: 200,
+      json: () =>
+        Promise.resolve({
+          message: "Email verification successful, your email has been successfully verified"
+        })
+    }) as Promise<Response>
+  );
+});;
+
 describe("Verify Component", () => {
   it("renders without crashing", async () => {
-    render(<Verify params={{}} searchParams={{}} />);
-
-    const messageElement = await screen.findByText("Cannot access");
-    expect(messageElement).toBeInTheDocument();
-  });
-
-  it("renders message with search params", async () => {
-    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
-
-    const messageElement = await screen.findByText(
-      "Email verification successful, your email has been successfully verified",
-    );
-    expect(messageElement).toBeInTheDocument();
-  });
-
-  it("renders message if fetch succeeds", async () => {
-    global.fetch = jest.fn().mockResolvedValueOnce({
-      json: async () => ({
-        message:
-          "Email verification successful, your email has been successfully verified",
+    render(
+      await Verify({
+        params: "",
+        searchParams: { token: "Token" },
       }),
-      status: 200,
-    });
-
-    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
-
-    const messageElement = await screen.findByText(
-      "Email verification successful, your email has been successfully verified",
     );
-    expect(messageElement).toBeInTheDocument();
+    expect(await screen.findByText("Email verification successful, your email has been successfully verified")).toBeVisible();
   });
 
-  it("renders message if fetch fails", async () => {
-    global.fetch = jest.fn().mockResolvedValueOnce({
-      json: async () => ({ message: "Email already verified / invalid token" }),
-      status: 400,
-    });
-
-    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
-
-    const messageElement = await screen.findByText(
-      "Email already verified / invalid token",
+  it("renders error message if token is missing", async () => {
+    render(
+      await Verify({
+        params: "",
+        searchParams: {},
+      }),
     );
-("Email already verified / invalid token");
-("Email already verified / invalid token");
-    expect(messageElement).toBeInTheDocument();
+    expect(await screen.findByText("Cannot access")).toBeVisible();
+  });
+
+  it("renders error message if API call fails", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        status: 400,
+        json: () =>
+          Promise.resolve({
+            message: "Email already verified / invalid token"
+          })
+      }) as Promise<Response>
+    );
+
+    render(
+      await Verify({
+        params: "",
+        searchParams: {token:"Token"},
+      }),
+    );
+    expect( await screen.findByText("Email already verified / invalid token")).toBeVisible();
+  });
+
+  it("renders error message if API call throws an error", async () => {
+    global.fetch = jest.fn(() => Promise.reject("API Error"));
+
+    render(
+      await Verify({
+        params: "",
+        searchParams: {token:"Token"},
+      }),
+    );
+    expect(await screen.findByText("API Error")).toBeVisible();
   });
 });
