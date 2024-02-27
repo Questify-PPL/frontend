a55e857fcd2851ff70b8c044fec2e2efeba36d8f
Kenneth Ferdinand <kenneth.ferdinand@ui.ac.id>
2024-02-27 15:46:45 +0700

    Email verification finished

---
+++ __tests__/register/verification/verify.test.tsx
@@ -0,0 +1,50 @@
+import Verify from "@/app/register/verification/page";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from 'react';
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
+describe("Verify Component", () => {
+  it("renders without crashing", async () => {
+    render(<Verify params={{}} searchParams={{}} />);
+
+    const messageElement = await screen.findByText("Cannot access");
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  it("renders message with search params", async () => {
+    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
+
+    const messageElement = await screen.findByText("Email verification successful, your email has been successfully verified");
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  it("renders message if fetch succeeds", async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      json: async () => ({ message: "Email verification successful, your email has been successfully verified" }),
+      status: 200,
+    });
+
+    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
+
+    const messageElement = await screen.findByText("Email verification successful, your email has been successfully verified");
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  it("renders message if fetch fails", async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      json: async () => ({ message: "Email already verified / invalid token" }),
+      status: 400,
+    });
+
+    render(<Verify params={{}} searchParams={{ token: "Token" }} />);
+
+    const messageElement = await screen.findByText("Email already verified / invalid token");
+    expect(messageElement).toBeInTheDocument();
+  });
+});
---
+++ app/register/verification/layout.tsx
@@ -0,0 +1,13 @@
+import { TriviaForm } from "@/components/auth";
+
+export default function RegisterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="w-full flex flex-col md:flex-row items-center justify-center h-full min-h-fit">
+      {children}
+    </section>
+  );
+}
