22c3b35733d8d9fc29c29a6c938dd0db88c6f4c7
Ruben Tanoey <92169008+rubentanoey@users.noreply.github.com>
2024-03-04 01:56:01 +0700

    red: create test for answer question 1

--- __tests__/additional-info/additional-info.test.tsx
+++ __tests__/additional-info/additional-info.test.tsx
@@ -18,3 +18,23 @@ test("renders the first step of the form", () => {
 
   expect(screen.getByText("Start")).toBeInTheDocument();
 });
+
+
+test("allows user to progress through the form steps", async () => {
+  render(<Form />);
+  fireEvent.click(screen.getByText("Start"));
+  await screen.findByText("Question 1");
+  expect(screen.getByText("Question 1")).toBeInTheDocument();
+  expect(screen.getByText("What's your name?")).toBeInTheDocument();
+  expect(screen.getByText("Fill with your full name.")).toBeInTheDocument();
+
+  const nameInput = screen.getByPlaceholderText(
+    "Your answer here",
+  ) as HTMLInputElement;
+  fireEvent.change(nameInput, { target: { value: "John Doe" } });
+  fireEvent.click(screen.getByText("Next"));
+
+  await screen.findByText("Question 2");
+  expect(screen.getByText("Question 2")).toBeInTheDocument();
+});
+
