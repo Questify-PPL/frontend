ad03250ede1bb9b475547c2d6066e014808c3a90
Kenneth Ferdinand <kenneth.ferdinand@ui.ac.id>
2024-04-23 16:04:13 +0700

    green:create implementation for Notification Card

---
+++ components/notification/NotificationCard.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+interface NotificationProps {
+  title: string;
+  message: string;
+}
+
+const NotificationMessage: React.FC<NotificationProps> = ({
+  title,
+  message,
+}) => {
+  return (
+    <div className="p-2">
+      <div className="flex justify-between">
+        <span className="text-m font-bold ">{title}</span>
+      </div>
+      <div>
+        <span className="text-xs ">{message}</span>
+      </div>
+    </div>
+  );
+};
+
+const NotificationCard: React.FC<{ title: string; message: string }> = ({
+  title,
+  message,
+}) => {
+  return (
+    <motion.div
+      className={`relative md:w-1/3 sm:w-24 h-fit gap-2`}
+      data-testid="notification-card"
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Card>
+        <NotificationMessage title={title} message={message} />
+      </Card>
+    </motion.div>
+  );
+};
+
+export default NotificationCard;
