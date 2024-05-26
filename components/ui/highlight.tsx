e0db9d974a662c7db569157880549409d4956b96
eddoog <codewithalfredo@gmail.com>
2024-05-26 13:53:11 +0700

    chores: format code

--- components/ui/highlight.tsx
+++ components/ui/highlight.tsx
@@ -32,7 +32,7 @@ export const Highlight = ({
       }}
       className={cn(
         `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-[rgb(63_114_123/0.9568160)_64%] to-[rgb(37_170_192/1)_99%] dark:from-indigo-500 dark:to-purple-500`,
-        className
+        className,
       )}
     >
       {children}
