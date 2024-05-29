2b6da0baa85d41a99634fa279e3f79244029c30e
Lyzander Marciano Andrylie <lyzander.marciano@ui.ac.id>
2024-05-29 07:58:50 +0700

    Improving Public Route Accessibility

--- components/auth/TriviaForm.tsx
+++ components/auth/TriviaForm.tsx
@@ -2,11 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { TriviaSchema } from "@/lib/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useToast } from "../ui/use-toast";
+import { Input } from "../ui/input";
 
 export function TriviaForm() {
   const { handleSubmit, setValue } = useForm<TriviaSchema>({
@@ -40,65 +40,78 @@ export function TriviaForm() {
     <form
       onSubmit={handleSubmit(onSubmit)}
       className="flex-col items-center justify-center md:flex hidden md:w-[270px] md:p-[24px] lg:w-[380px] lg:p-[40px] gap-[32px] rounded-[6px] h-fit bg-white"
+      aria-label="trivia form"
     >
       <div className="flex items-center gap-[6px] self-stretch">
         <span className="w-[4px] self-strech h-full rounded-[2px] bg-destructive"></span>
         <p className="text-primary text-xs font-medium capitalize">Trivia</p>
       </div>
       <div className="flex flex-col justify-center items-start gap-4 self-stretch">
-        <Label className="text-sm font-semibold text-black">
-          What sport was featured on the first curved U.S. coin in 2014?
-        </Label>
-        <RadioGroup className="flex flex-col gap-2">
+        <fieldset className="flex flex-col gap-2">
+          <legend className="text-sm font-semibold text-black mb-4">
+            What sport was featured on the first curved U.S. coin in 2014?
+          </legend>
           <div className="flex items-center self-stretch gap-2">
-            <RadioGroupItem
+            <Input
+              type="radio"
+              name="answer"
               value="Basketball"
               id="option-one"
-              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1]"
+              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1] accent-[#47737a]"
               onClick={() => {
                 setValue("answer", "Basketball");
               }}
+              tabIndex={-1}
             />
             <Label htmlFor="option-one">Basketball</Label>
           </div>
           <div className="flex items-center self-stretch gap-2">
-            <RadioGroupItem
+            <Input
+              type="radio"
+              name="answer"
               value="Baseball"
               id="option-two"
-              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1]"
+              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1] accent-[#47737a]"
               onClick={() => {
                 setValue("answer", "Baseball");
               }}
+              tabIndex={-1}
             />
             <Label htmlFor="option-two">Baseball</Label>
           </div>
           <div className="flex items-center self-stretch gap-2">
-            <RadioGroupItem
+            <Input
+              type="radio"
+              name="answer"
               value="Football"
               id="option-three"
-              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1]"
+              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1] accent-[#47737a]"
               onClick={() => {
                 setValue("answer", "Football");
               }}
+              tabIndex={-1}
             />
 
             <Label htmlFor="option-three">Football</Label>
           </div>
 
           <div className="flex items-center self-stretch gap-2">
-            <RadioGroupItem
+            <Input
+              type="radio"
+              name="answer"
               value="Hockey"
               id="option-four"
-              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1]"
+              className="h-4 w-4 rounded-[2.285px] border-[1px] border-solid border-[#CDDDE1] accent-[#47737a]"
               onClick={() => {
                 setValue("answer", "Hockey");
               }}
+              tabIndex={-1}
             />
             <Label htmlFor="option-four">Hockey</Label>
           </div>
-        </RadioGroup>
+        </fieldset>
       </div>
-      <Button type="submit" className="w-full bg-[#606F75]">
+      <Button type="submit" className="w-full bg-[#606F75]" tabIndex={-1}>
         Submit
       </Button>
     </form>
--- components/landing-page/End.tsx
+++ components/landing-page/End.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 
 export function End() {
   return (
-    <section className="flex flex-col items-center justify-center w-full px-2 py-6 lg:py-12">
+    <section
+      className="flex flex-col items-center justify-center w-full px-2 py-6 lg:py-12"
+      aria-label="end"
+    >
       <Head
         content={
           "Unlock academic success with our streamlined form builder and distributor"
